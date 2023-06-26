455ade4bd8c9e40e292afafbf4a158192fac36d4
AyushBabbar <76393139+AyushBabbar@users.noreply.github.com>
2023-06-27 03:09:23 +0530

    Update CoinsTable.js

--- src/components/CoinsTable.js
+++ src/components/CoinsTable.js
@@ -78,8 +78,8 @@ export default function CoinsTable() {
   const handleSearch = () => {
     return coins.filter(
       (coin) =>
-        coin.name.toLowerCase().includes(search) ||
-        coin.symbol.toLowerCase().includes(search)
+        coin.name.toLowerCase().includes(search..toLowerCase()) ||
+        coin.symbol.toLowerCase().includes(search..toLowerCase())
     );
   };
 
